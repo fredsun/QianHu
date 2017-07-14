99c102d1c1bdd821e2d2101836ce3c6663a037f0
sunxl <fredsun1206@gmail.com>
2017-07-14 16:16:42 +0800

    加入最基本的网络请求功能

--- app/src/main/java/org/qianhu/app/ui/fragment/NotificationFragment.java
+++ app/src/main/java/org/qianhu/app/ui/fragment/NotificationFragment.java
@@ -4,18 +4,87 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
 
 import org.qianhu.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by fred on 2017/5/26.
  */
 
 public class NotificationFragment extends LazyLoadFragment {
-
+    TextView tv_get_result;
     @Override
     public void lazyLoad() {
         Log.i("notificationFragment","lazyLoad");
+        tv_get_result = (TextView) findViewById(R.id.tv_get_result);
+        Button btn_get = (Button) findViewById(R.id.btn_get);
+        btn_get.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRequestWithHttpURLConnection();
+            }
+        });
+    }
+
+    private void sendRequestWithHttpURLConnection() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpsURLConnection connection = null;
+                BufferedReader reader = null;
+                try{
+                    URL url = new URL("https://www.baidu.com");
+                    connection = (HttpsURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    InputStream in = connection.getInputStream();
+                    reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while((line = reader.readLine()) != null){
+                        response.append(line);
+                    }
+                    showResponse(response.toString());
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally{
+                    if (reader != null){
+                        try{
+                            reader.close();
+                        }catch (IOException e){
+                            e.printStackTrace();
+                        }
+                    }
+                    if (connection != null){
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void showResponse(final String s) {
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                tv_get_result.setText(s);
+            }
+        });
     }
 
     @Override
@@ -36,63 +105,7 @@ public class NotificationFragment extends LazyLoadFragment {
         Log.i("notificationFragment","DestroyView");
     }
 
-    @Override
-    public void setUserVisibleHint(boolean isVisibleToUser) {
-        super.setUserVisibleHint(isVisibleToUser);
-        Log.i("notificationFragment","setUserVisibleHint: "+isVisibleToUser);
-    }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.i("notificationFragment","onResume");
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        Log.i("notificationFragment","onAttach");
-    }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.i("notificationFragment","onCreate");
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        Log.i("notificationFragment","onActivityCreated");
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        Log.i("notificationFragment","onStart");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Log.i("notificationFragment","onPause");
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        Log.i("notificationFragment","onStop");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.i("notificationFragment","onDestroy");
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        Log.i("notificationFragment","onDetach");
-    }
 }
