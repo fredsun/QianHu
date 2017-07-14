34983bae9080585077a45642e27fbba294cd6b04
sunxl <fredsun1206@gmail.com>
2017-07-14 15:10:43 +0800

    加入浏览相册功能

--- app/src/main/java/org/qianhu/app/ui/fragment/HomeFragment.java
+++ app/src/main/java/org/qianhu/app/ui/fragment/HomeFragment.java
@@ -1,26 +1,34 @@
 package org.qianhu.app.ui.fragment;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import org.litepal.crud.DataSupport;
@@ -33,20 +41,30 @@ import org.qianhu.adapter.FruitAdapter;
 import org.qianhu.database.Book;
 import org.qianhu.database.MyDatabaseHelper;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.jar.Manifest;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by fred on 2017/5/26.
  */
 
 public class HomeFragment extends LazyLoadFragment {
+    private ImageView picture;
+    private Uri imageUri;
+    public static final int TAKE_PHOTO = 1;
+    public static final int CHOOSE_PHOTO = 2;
+    private ImageView viewById3;
+
     @Override
     public void lazyLoad() {
-
-
+        Log.i("test","lazyload");
         View viewById = findViewById(R.id.btn_call);
         viewById.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,7 +92,6 @@ public class HomeFragment extends LazyLoadFragment {
                 Notification notification = new NotificationCompat.Builder(getContext())
                         .setContentTitle("this is a title")
                         .setContentText("this is a text")
-
                         .setWhen(System.currentTimeMillis())
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
@@ -87,6 +104,139 @@ public class HomeFragment extends LazyLoadFragment {
                 manager.notify(1,notification);
             }
         });
+
+        picture = (ImageView) findViewById(R.id.iv_photo);
+        viewById3 = (ImageView) findViewById(R.id.iv_photo);
+        View viewById2 = findViewById(R.id.btn_take_photo);
+        viewById2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File outputImage = new File(getContext().getExternalCacheDir(),"output_image.jpg");
+                try{
+                    if (outputImage.exists()){
+                        outputImage.delete();
+                    }
+                    outputImage.createNewFile();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+                if (Build.VERSION.SDK_INT >= 24){
+                    imageUri = FileProvider.getUriForFile(getContext(),
+                            "org.qianhu.fileprovider",
+                            outputImage);
+                }else {
+                    imageUri = Uri.fromFile(outputImage);
+                }
+                //启动相机
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, TAKE_PHOTO);
+            }
+        });
+
+        View viewById4 = findViewById(R.id.btn_sel_photo);
+        viewById4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(getContext(),
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+                } else {
+                 openAlbum();
+                }
+            }
+
+        });
+    }
+
+    private void openAlbum() {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_PHOTO);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case  TAKE_PHOTO:
+                if (resultCode == RESULT_OK){
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContext().getContentResolver().openInputStream(imageUri));
+                        picture.setImageBitmap(bitmap);
+                    }catch (FileNotFoundException e){
+                        e.printStackTrace();
+                    }
+                }
+                break;
+
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK){
+                    if (Build.VERSION.SDK_INT >= 19){
+                        handleImageOnKitKat(data);
+                    } else {
+                        handleImageBeforeKitKat(data);
+                    }
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void handleImageBeforeKitKat(Intent data) {
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri, null);
+        displayImage(imagePath);
+    }
+
+    @TargetApi(19)
+    private void handleImageOnKitKat(Intent data){
+        String imagePath = null;
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(getContext(),uri)){
+            //如果是document类型的uri，则通过document id处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())){
+                String id = docId.split(":")[1];
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            }else if ("com.android,providers.downloads.documents".equals(uri.getAuthority())){
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),
+                        Long.valueOf(docId));
+                imagePath = getImagePath(contentUri, null);
+            }else if ("content".equalsIgnoreCase(uri.getScheme())){
+                //如果是content类型的uri
+                imagePath = getImagePath(uri, null);
+            }else if ("file".equalsIgnoreCase(uri.getScheme())){
+                //如果是file类型的uri
+                imagePath = uri.getPath();
+            }
+            displayImage(imagePath);
+        }
+    }
+
+    private void displayImage(String imagePath) {
+        if (imagePath != null){
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            picture.setImageBitmap(bitmap);
+        }else {
+            Toast.makeText(getContext(), "failed to get image", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String getImagePath(Uri uri, String selection) {
+        String path = null;
+        //通过Uri和selection来获得图片的真实路径
+        Cursor cursor = getContext().getContentResolver().query(uri, null, selection, null, null);
+        if (cursor != null){
+            if (cursor.moveToFirst()){
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
     }
 
     private void call() {
@@ -105,7 +255,8 @@ public class HomeFragment extends LazyLoadFragment {
         switch (requestCode){
             case 1:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    call();
+//                    call();
+                    openAlbum();
                 }else {
                     Toast.makeText(getContext(), "You denied the permission", Toast.LENGTH_SHORT).show();
                 }
