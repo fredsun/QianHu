595d469c9fbe1328a0d01fad0fe4dd610bc4d6e5
sunxl <fredsun1206@gmail.com>
2017-07-17 10:56:02 +0800

    加入pull解析

--- app/src/main/java/org/qianhu/app/ui/fragment/NotificationFragment.java
+++ app/src/main/java/org/qianhu/app/ui/fragment/NotificationFragment.java
@@ -11,15 +11,24 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import org.qianhu.R;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import static java.sql.DriverManager.println;
 
 /**
  * Created by fred on 2017/5/26.
@@ -41,41 +50,64 @@ public class NotificationFragment extends LazyLoadFragment {
     }
 
     private void sendRequestWithHttpURLConnection() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                HttpsURLConnection connection = null;
-                BufferedReader reader = null;
-                try{
-                    URL url = new URL("https://www.baidu.com");
-                    connection = (HttpsURLConnection) url.openConnection();
-                    connection.setRequestMethod("GET");
-                    connection.setConnectTimeout(8000);
-                    connection.setReadTimeout(8000);
-                    InputStream in = connection.getInputStream();
-                    reader = new BufferedReader(new InputStreamReader(in));
-                    StringBuilder response = new StringBuilder();
-                    String line;
-                    while((line = reader.readLine()) != null){
-                        response.append(line);
-                    }
-                    showResponse(response.toString());
-                }catch (Exception e){
-                    e.printStackTrace();
-                }finally{
-                    if (reader != null){
-                        try{
-                            reader.close();
-                        }catch (IOException e){
-                            e.printStackTrace();
+       new Thread(new Runnable() {
+           @Override
+           public void run() {
+               try{
+                   OkHttpClient client = new OkHttpClient();
+                   Request request = new Request.Builder()
+                           .url("http://172.16.82.6/get_data.xml")
+                           .build();
+                   Response response = client.newCall(request).execute();
+                   String responseData = response.body().string();
+                   parseXMLWithPull(responseData);
+               }catch (Exception e){
+                   e.printStackTrace();
+               }
+           }
+       }).start();
+    }
+
+    private void parseXMLWithPull(String responseData) {
+        try{
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(responseData));
+            int eventType = xmlPullParser.getEventType();
+            String id = "";
+            String name = "";
+            String version = "";
+            while(eventType != XmlPullParser.END_DOCUMENT){
+                String nodeName = xmlPullParser.getName();
+                switch (eventType){
+                    //开始解析节点
+                    case XmlPullParser.START_TAG:{
+                        if ("id".equals(nodeName)){
+                            id = xmlPullParser.nextText();
+                        } else if ("name".equals(nodeName)){
+                            name = xmlPullParser.nextText();
+                        } else if ("version".equals(nodeName)){
+                            version = xmlPullParser.nextText();
                         }
+                        break;
                     }
-                    if (connection != null){
-                        connection.disconnect();
+
+                    case XmlPullParser.END_TAG: {
+                        if ("app".equals(nodeName)) {
+                            Log.i("okhttp", "id is" + id);
+                            Log.i("okhttp", "name is" + name);
+                            Log.i("okhttp", "version is" + version);
+                        }
+                        break;
                     }
+                    default:
+                        break;
                 }
+                eventType = xmlPullParser.next();
             }
-        }).start();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     private void showResponse(final String s) {
